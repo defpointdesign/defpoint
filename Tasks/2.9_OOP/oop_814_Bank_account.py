9a68f25db6ab21a4a31b8661343c55ae64b77f2a
oddsname <35614059+oddsname@users.noreply.github.com>
2023-06-27 14:20:18 +0300

    Updated oop_814_Bank_account.py, replaced class field on class property

--- Tasks/2.9_OOP/oop_814_Bank_account.py
+++ Tasks/2.9_OOP/oop_814_Bank_account.py
@@ -1,26 +1,24 @@
 class Bank:
     '''Banking operations'''
-    balance = 0
     def __init__(self, account_balance):
         self.balance = account_balance
-        Bank.balance += self.balance
         print('I open a bank account')
 
     def deposit(self):
         '''Deposit money into a bank account'''
         add_dep= int(input('I deposit to your account: '))
-        Bank.balance += add_dep
+        self.balance += add_dep
         print('On the account: ', Bank.balance)
 
     def withdraw (self):
         '''Withdraw money from a bank account'''
         take_dep = int(input('I withdraw from your bank account: '))
-        if take_dep <= Bank.balance:
-            Bank.balance -= take_dep
-            print('On the account:', Bank.balance)
+        if take_dep <= self.balance:
+            self.balance -= take_dep
+            print('On the account:', self.balance)
         else:
             print ('Error: not enough money')
-            print('On the account:', Bank.balance)
+            print('On the account:', self.balance)
 
 
 
